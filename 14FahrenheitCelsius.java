28479c44587bf5226e3ae9c1584a86fefc5d7eed
chiragsinghbaghel <chiragsinghbaghel8@gmail.com>
2025-07-11 08:12:41 +0530

    Add files via upload

--- 14FahrenheitCelsius.java
+++ 14FahrenheitCelsius.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 class FahrenheitCelsius {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Welcome to Temperature converter\n");
+        System.out.print("Enter your temp in F:");
+        float fah =input.nextFloat();
+        float cel=(fah-32)*5/9;
+        System.out.println("Your temperature is :"+cel+"c");
+
     }
 }
